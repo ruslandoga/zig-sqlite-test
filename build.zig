ff389560c222f0f24ebf31100cf70d05079c8570
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-11-02 15:01:55 +0700

    add "open and query" test

--- build.zig
+++ build.zig
@@ -6,10 +6,16 @@ pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
 
     const lib = b.addStaticLibrary("zig-sqlite", "src/main.zig");
+    lib.addCSourceFile("c_src/sqlite3.c", &[_][]const u8{"-std=c99"});
+    lib.linkLibC();
+    lib.addIncludePath("c_src");
     lib.setBuildMode(mode);
     lib.install();
 
     const main_tests = b.addTest("src/main.zig");
+    main_tests.addCSourceFile("c_src/sqlite3.c", &[_][]const u8{"-std=c99"});
+    main_tests.linkLibC();
+    main_tests.addIncludePath("c_src");
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,27 @@
+const c = @cImport(@cInclude("../c_src/sqlite3.h"));
+
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+test "open and query" {
+    std.debug.print("\nsqlite3 version: {s}\n", .{c.sqlite3_libversion()});
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+    var db: ?*c.sqlite3 = undefined;
+    var rc: c_int = undefined;
+    rc = c.sqlite3_open(":memory:", &db);
+    try testing.expectEqual(rc, c.SQLITE_OK);
+
+    var stmt: ?*c.sqlite3_stmt = undefined;
+    rc = c.sqlite3_prepare_v2(db, "select sqlite_version()", -1, &stmt, 0);
+    try testing.expectEqual(rc, c.SQLITE_OK);
+
+    rc = c.sqlite3_step(stmt);
+    try testing.expectEqual(rc, c.SQLITE_ROW);
+    std.debug.print("selected version: {s}\n", .{c.sqlite3_column_text(stmt, 0)});
+
+    rc = c.sqlite3_finalize(stmt);
+    try testing.expectEqual(rc, c.SQLITE_OK);
+
+    rc = c.sqlite3_close(db);
+    try testing.expectEqual(rc, c.SQLITE_OK);
 }
